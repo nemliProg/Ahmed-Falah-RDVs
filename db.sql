a479ec94925650463ce4c5e555d24e2c58e0f261
nemliProg <younesbuzz@gmail.com>
2022-03-28 15:23:20 +0200

    creation de la bae de donnes

---
+++ db.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE AFRDVS;
+
+CREATE TABLE Client (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(50) NOT NULL,
+  prenom VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  prefession VARCHAR(50) NOT NULL,
+  ref VARCHAR(255) NOT NULL UNIQUE,
+  id_admin INT REFERENCES id(Admin)
+)
+
+
+CREATE TABLE Admin (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(50) NOT NULL,
+  prenom VARCHAR(50) NOT NULL,
+  ref VARCHAR(255) NOT NULL UNIQUE
+)
+
+CREATE TABLE RDVs (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  sujet_rdv VARCHAR(255) NOT NULL,
+  date_rdv DATE NOT NULL,
+  creneau_rdv INT NOT NULL CHECK(creneau_rdv > 0 && creneau_rdv <= 5),
+  id_client INT REFERENCES id(Client),
+  id_admin INT REFERENCES id(Admin)
+)
