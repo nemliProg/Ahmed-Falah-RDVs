f0951430f9ab3d0ceb4515b5976d62c969d57ea8
nemliProg <younesbuzz@gmail.com>
2022-03-28 16:46:42 +0200

    db created

--- db.sql
+++ db.sql
@@ -2,17 +2,6 @@ CREATE DATABASE AFRDVS;
 
 USE AFRDVS;
 
-CREATE TABLE Client (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  nom VARCHAR(50) NOT NULL,
-  prenom VARCHAR(50) NOT NULL,
-  age INT NOT NULL,
-  prefession VARCHAR(50) NOT NULL,
-  ref VARCHAR(255) NOT NULL UNIQUE,
-  id_admin INT REFERENCES Admin(id)
-);
-
-
 CREATE TABLE Admin (
   id INT PRIMARY KEY AUTO_INCREMENT,
   nom VARCHAR(50) NOT NULL,
@@ -20,11 +9,25 @@ CREATE TABLE Admin (
   ref VARCHAR(255) NOT NULL UNIQUE
 );
 
+CREATE TABLE Client (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(50) NOT NULL,
+  prenom VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  prefession VARCHAR(50) NOT NULL,
+  ref VARCHAR(255) NOT NULL UNIQUE,
+  idAdmin INT,
+  CONSTRAINT fk_admin FOREIGN KEY (idAdmin) REFERENCES Admin(id)
+);
+
+
 CREATE TABLE RDVs (
   id INT PRIMARY KEY AUTO_INCREMENT,
   sujet_rdv VARCHAR(255) NOT NULL,
   date_rdv DATE NOT NULL,
   creneau_rdv INT NOT NULL CHECK(creneau_rdv > 0 && creneau_rdv <= 5),
-  id_client INT REFERENCES Client(id),
-  id_admin INT REFERENCES Admin(id)
+  idClient INT ,
+  idAdmin INT , 
+  CONSTRAINT fk_rdv_client FOREIGN KEY (idClient) REFERENCES Client(id),
+  CONSTRAINT fk_rdv_admin FOREIGN KEY (idAdmin) REFERENCES Admin(id)
 );
