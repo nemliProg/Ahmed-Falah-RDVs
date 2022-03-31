f48d0a9e4f2031c7bd2577eaba0adab740d5b295
unknown <doufarejawad23@gmail.com>
2022-03-31 10:23:47 +0000

    adding calendar

--- afrdvs/src/views/Dashboard.vue
+++ afrdvs/src/views/Dashboard.vue
@@ -1,25 +1,120 @@
 <template>
-  <h1>Calendar</h1>
+<button @click="incrementTheOneDay()">Click</button>
+  <div class="container">
+    <div id="weekdays">
+     <div>Su</div>
+     <div>Mo</div>
+     <div>Tu</div>
+     <div>We</div>
+     <div>Th</div>
+     <div>Fr</div>
+     <div>Sa</div>
+    </div>
+    <div ref="calendar" id="calendar">
+          
+    </div>
+  </div>
   
 </template>
 
-<script>
+<script>import { onMounted } from "vue";
+
 export default {
   name : "Dashboard",
   data(){
     return {
-
+       oneMonth : 0
     }
   },
   components : {
 
+  },
+  methods : {
+     displayCalenderDays(){
+  const date = new Date();
+  //Date class => By default gives cuurent date but you could change that if you define a date in the constructor
+  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  //Current Date infos => S
+  const day =  date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  //The date of every month
+  const firstDayOfTheMonth = new Date(year,month,1);
+  const daysInCurrentMonth = new Date(year,month+1,0).getDate();
+  const dateString = firstDayOfTheMonth.toLocaleDateString('en-us',{
+     weekday : 'long',
+     year : 'numeric',
+     month : 'numeric',
+     day : 'numeric',
+  })
+  const pastDays = weekdays.indexOf(dateString.split(', ')[0]);
+  for(let i = 1;i<=pastDays + daysInCurrentMonth;i++){
+    const daySquare = document.createElement('div');
+    if(i > pastDays){
+      daySquare.innerText = i - pastDays;
+      daySquare.classList.add("day");
+      if((i - pastDays)==day && firstDayOfTheMonth.getMonth() == month){
+           daySquare.style.color = "#247BA0";
+      }else if((i - pastDays) < day && firstDayOfTheMonth.getMonth() == month){
+           daySquare.style.backgroundColor = "#DDD";
+      }
+    }
+    else{
+       daySquare.classList.add("emptyDay");
+    }
+    console.log(daySquare);
+    this.$refs.calendar.appendChild(daySquare);
   }
+  },
+  incrementTheOneDay(){
+    console.log(this.oneDay);
+    this.oneDay = this.oneDay + 1;
+    console.log(this.oneDay);
+    
+  }
+  },
+  mounted(){
+      this.displayCalenderDays();
+  }
+  
 }
 </script>
 
 <style lang="scss">
-  h1 {
-    text-align: center;
+.container{
+  width: 280px;
+}
+#weekdays {
+  width: 100%;
+  display: flex;
+  color: #247BA0;
+  
+  margin-top: 20px;
+}
+#weekdays div {
+  width: 40px;
+ 
+  text-align: center;
+}
+#calendar{
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.day{
+  width: 40px;
+  cursor: pointer;
+  text-align: center;
+  
+}
+.day:hover{
+  background-color: #247BA0;
+}
+.emptyDay{
+  width: 40px;
+  cursor: pointer;
+  text-align: center;
 
-  }
+}
+
 </style>
